f11e0a2eaff35ff7116a9a21b657e2dbbed82970
andreyevsilva <andreyev2011@hotmail.com>
2020-07-24 04:52:04 -0300

    Mostrando Resultado

--- matador-de-monstro/app.js
+++ matador-de-monstro/app.js
@@ -1,11 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        playerLife:19,
-        monsterLife:45,
+        playerLife:0,
+        monsterLife:100,
     },
     computed: {
-
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0;
+        }
     },
     method: {
 
