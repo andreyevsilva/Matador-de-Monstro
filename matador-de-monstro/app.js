431c26e4b565eea4b96c8cf64ada912662a7a738
andreyevsilva <andreyev2011@hotmail.com>
2020-07-25 18:13:08 -0300

    Implementando Atacar e Curar

--- matador-de-monstro/app.js
+++ matador-de-monstro/app.js
@@ -17,8 +17,52 @@ new Vue({
             this.playerLife = 100;
             this.monsterLife = 100;
         },
+        /*Para ataques normais e especiais */
+        attack(especial){
+            this.hurt('monsterLife',5,10,especial,'Player','Monstro','player');
+            if(this.monsterLife > 0){
+                this.hurt('playerLife',7,12,false,'Monstro','Player','monster');
+            }
+        },
+        /*Machucar*/
+        hurt(attr,min,max,especial,source,target,classe){
+            const plus = especial ? 5 : 0;
+            const hurt = this.getRandom(min+plus,max+plus);
+
+            /*Caso a subração da vida do jogar pelo ataque, seja negativa, o valor fica 0, 
+            assim evita valor negativo */
+            this[attr] = Math.max(this[attr] - hurt ,0);
+
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`,classe)
+        },
+        /*Curar e atacar */
+        healAndHurt(){
+            this.heal(10,15);
+            this.hurt('playerLife',7,12,false);
+        },
+        /*Curar*/
+        heal(min,max){
+            const heal = this.getRandom(min,max);
+            this.playerLife = Math.min(this.playerLife+heal,100);
+        },
+        /*Registrar Logs
+
+         Obs:usei 'classe', para não ter problemas com a palavra 'class'
+        */
+        registerLog(text,classe){
+            this.logs.unshift({text,classe})
+        },
+        getRandom(min,max){
+            const value = Math.random() * (max - min) + min;
+            return Math.round(value);
+        }
     },
     watch: {
-
+        hasResult(value){
+            if (value)
+                this.running = false;
+            else
+                this.running = true;
+        }
     }
 });
